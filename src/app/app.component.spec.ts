ba21c0ee847b9a5c9f678de2db4aa6f570b4f085
pankowskitomasz <pankowski.email@gmail.com>
2022-02-26 21:02:52 +0200

    initial state

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'learnenglish'`, () => {
+  it(`should have as title 'babysitting'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learnenglish');
+    expect(app.title).toEqual('babysitting');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('learnenglish app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('babysitting app is running!');
   });
 });
